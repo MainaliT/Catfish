f193217d3962f794bde7baade8bde648eb4cc6c9
Tek Mainali <parulmainali@gmail.com>
2016-01-07 19:52:15 -0500

    Catfish
    Using sealed method,

---
+++ Catfish/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish
+{   
+    //this is a sealed class, it will not be able to inherit to another form 
+    public sealed class Circle:Shape
+    {
+        private const float PI = 3.14159f;
+        public float Radius;
+        public float Area;
+
+        public Circle()
+        {
+            Radius = 1.0f;
+
+        }
+
+            public override float ComputeArea()
+            {
+                Area = PI * Radius * Radius;
+
+                return Area;
+            }
+
+        }
+
+    }
+
+
+
+
+
+
---
+++ Catfish/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catfish
+{
+   public sealed class Square : Shape
+    {
+        public float Side;
+        public float Area;
+
+
+        public Square()
+        {
+            Side = 1.0f;
+        }
+
+
+        public override float ComputeArea()
+        {
+             Area = Side * Side;
+
+            return Area;
+        }
+    }
+}
